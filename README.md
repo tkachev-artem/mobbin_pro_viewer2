98995593531ac38dca9cb6613e9896d7d4a0b996
Romanbot4 <tice.lover1500@gmail.com>
2024-03-24 19:41:14 +0630

    init

---
+++ README.md
@@ -0,0 +1,34 @@
+# Mobbin Pro Viewer
+
+## Overview
+
+The Mobbin Pro Viewer is a script that allows users to browse and view screenshots curated by mobbin.com. This script provides a convenient way to explore the latest design trends and inspirations from the industry-leading designers and companies featured on mobbin.com.
+
+
+## Setup
+
+To run the Mobbin Pro Viewer script, follow these steps:
+
+1. Clone this repository to your local machine.
+2. Go to your chromium browser(such as Chrome, Edge or Opera)'s extension page
+3. Enable developer mode & click load upacked.
+4. Then select the cloned repository folder. That's it. 
+
+## Usage Guidelines
+
+This script is provided for educational purposes only and should not be used to bypass paywalls or violate the terms of service of mobbin.com or any other website. Users are encouraged to respect the rights of content creators and adhere to ethical principles when using the script.
+
+## Disclaimer
+
+The Mobbin Pro Viewer script is provided "as is" and without warranty. The author is not liable for any misuse or legal consequences resulting from the use of the script.
+
+## Contributions
+
+
+## Contributing
+Contributions are welcome! Feel free to open issues or submit pull requests.
+
+## License
+This project is licensed under the [MIT License](LICENSE.md).
+
+Enjoy coding! 🍔🍕🚀
---
+++ content-script.js
@@ -0,0 +1,53 @@
+let timer;
+window.addEventListener('scroll', function() {
+    clearTimeout(timer);
+    timer = setTimeout(main, 200);
+});
+
+function main() {
+    function queryNodes(pattern) {
+        let elems = document.querySelectorAll('img')
+        return Array.from(elems).filter((elem) => {
+            return pattern.test(elem.src);
+        })
+    }
+
+    function extractParam(elems, pattern) {
+        for (const elem of elems) {
+            let param = pattern.exec(elem.src)
+            if (param != null) return param[1]
+        }
+        return null;
+    }
+
+    function hideOverlay() {
+        let stickyElem = document.querySelector("main aside.sticky")
+        if (stickyElem != null) stickyElem.style.display = 'none'
+    }
+
+    function replaceParam(elem, param) {
+        let startIndex = elem.src.indexOf("&image=") + "&image=".length;
+        let altered = (elem.src.substring(0, startIndex) + param).replace(/(?<=f=webp&w=)\d+(?=&q)/, 1920) + "&gravity=bottom"
+        elem.src = altered
+        elem.closest('div')?.removeAttribute('class')
+    }
+
+    function execInjection(elems, param) {
+        Array.from(elems).forEach(elem => {
+            replaceParam(elem, param)
+        });
+    }
+
+    function queryAndPro(pattern) {
+        hideOverlay();
+
+        let elems = queryNodes(pattern)
+        if (elems.length === 0) return;
+        
+        let param = extractParam(elems, pattern)
+        execInjection(elems, param)
+    }
+
+    let pattern = /^https:\/\/bytescale\.mobbin\.com\S+\/image\/content\/app_screens\/\S{8}-\S{4}-\S{4}-\S{4}-\S{12}\.\S{3,4}\?f=\S{3,4}\&w=\d{2,4}\&q=\d{2,3}&fit=shrink-cover&extend-bottom=120&image=([^&]+)\&gravity=bottom$/
+    queryAndPro(pattern);
+}
